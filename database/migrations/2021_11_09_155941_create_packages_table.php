2742b13e6810752c485f186a531d3b1f7f86eb2f
manfred-commits <manfredipiraino@gmail.com>
2021-11-09 17:07:37 +0100

    first commit

---
+++ database/migrations/2021_11_09_155941_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('number_of_locations');
+            $table->id('numero_persone');
+            $table->string('description');
+            $table->float('price', 7,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
